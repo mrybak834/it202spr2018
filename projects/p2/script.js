37b96b1d2d0781ee09e7f1551729106d790eed6e
unknown <mrybak3@uic.edu>
2018-03-04 23:32:57 -0600

    Working map

--- projects/p2/script.js
+++ projects/p2/script.js
@@ -9,9 +9,13 @@ $( document ).ready(function() {
         $("#input").toggle();
         $("#map").toggle();
 
-        selectedItems.forEach(function(element){
-            $("#map").append("<p>"+ element +"<p>")
-        });
+        // selectedItems.forEach(function(element){
+        //     $("#map").append("<p>"+ element +"</p>")
+        // });
+
+        for (i = 0; i < 20; i++) { 
+            $("#map").append("<p>aasdfasdfasdfasfasdfffffffffffffffffffffff<p>")
+        }
     });
 
     // Get school type list
@@ -34,8 +38,40 @@ $( document ).ready(function() {
     })
     .trigger( "change" );
 
+    $.ajax({
+        url: "https://data.cityofchicago.org/resource/qjqj-a2d9.json",
+        method: 'GET',
+    }).done(function (result) {
+        $.each(result, function (key, value) {
+            console.log("Lat: " + value.latitude + ", Long: " + value.longitude);
+            label = "Lat: " + value.latitude + ", Long: " + value.longitude;
+            var myLatLng = { lat: parseFloat(value.latitude), lng: parseFloat(value.longitude) };
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map
+            });
+
+            var infowindow = new google.maps.InfoWindow({
+                content: label
+            });
+
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+
+        });
+
+    }).fail(function (err) {
+        throw err;
+    });
 
 
-
-    
 });
+
+function initMap() {
+    var Chicago = { lat: 41.8781, lng: -87.6298 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: Chicago
+    });
+}
