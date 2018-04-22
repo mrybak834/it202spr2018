b6fc6eef11dc6819c2e23f8297f560b713a3e923
Marek Rybakiewicz <mrybak3@uic.edu>
2018-04-22 07:43:23 -0500

    starts

---
+++ projects/big/static/scripts/script.js
@@ -0,0 +1,9 @@
+// https://api.tumblr.com/v2/blog/blackpplluvtshirts.tumblr.com/posts/photo?api_key=VV08xHlettWixKTtehz4B2h6jnBbIIB4mTsswmxLGb8eT8CXdh
+// https://api.tumblr.com/v2/blog/blackpplluvtshirts.tumblr.com/posts/text?api_key=VV08xHlettWixKTtehz4B2h6jnBbIIB4mTsswmxLGb8eT8CXdh&filter=text
+$(document).ready(function() {
+    console.log('ready!');
+
+    // $('#results').toggleClass('hidden');
+    // $('#start').toggleClass('hidden');
+    // $('#profile').toggle();
+});
