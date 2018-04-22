c5c1967a1c7c06a0b079ee41ec98a41dd1415bf7
Marek Rybakiewicz <mrybak3@uic.edu>
2018-04-22 08:53:52 -0500

    Getting user input

--- projects/big/static/scripts/script.js
+++ projects/big/static/scripts/script.js
@@ -2,8 +2,28 @@
 // https://api.tumblr.com/v2/blog/blackpplluvtshirts.tumblr.com/posts/text?api_key=VV08xHlettWixKTtehz4B2h6jnBbIIB4mTsswmxLGb8eT8CXdh&filter=text
 $(document).ready(function() {
     console.log('ready!');
+    var userBlog = '';
+    var compBlog = '';
 
     // $('#results').toggleClass('hidden');
     // $('#start').toggleClass('hidden');
     // $('#profile').toggle();
+
+    // Submit personal blog
+    $('#user-submit').click(function() {
+        userBlog = $('#user-blog-input').val();
+        console.log(userBlog);
+
+        // Hide personal, show comp text field
+        $('#user-form-field').toggleClass('hidden');
+        $('#comp-form-field').toggleClass('hidden');
+    });
+
+    $('#comp-submit').click(function() {
+        compBlog = $('#comp-blog-input').val();
+        console.log(compBlog);
+
+        // Hide comp and , show results page
+        $('#comp-form-field').toggleClass('hidden');
+    });
 });
