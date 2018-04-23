dd55d3f95f116980edbea14652443db83435b81f
Marek Rybakiewicz <mrybak3@uic.edu>
2018-04-22 23:57:12 -0500

    final submission big project

--- projects/big/static/scripts/serviceworker.js
+++ projects/big/static/scripts/serviceworker.js
@@ -1,23 +1,11 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(
-            function(registration) {
-                // Registration was successful
-                console.log(
-                    'ServiceWorker registration successful with scope: ',
-                    registration.scope
-                );
-            },
-            function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            }
-        );
-    });
-}
-
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = ['./style.css', './script.css'];
+var urlsToCache = [
+    './style.css',
+    './script.css',
+    '../../index.html',
+    '../assets/images/start-background.jpeg',
+    '../assets/libraries/d3pie.js'
+];
 
 self.addEventListener('install', function(event) {
     // Perform install steps
@@ -37,46 +25,6 @@ self.addEventListener('fetch', function(event) {
                 return response;
             }
             return fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-
-            // IMPORTANT: Clone the request. A request is a stream and
-            // can only be consumed once. Since we are consuming this
-            // once by cache and once by the browser for fetch, we need
-            // to clone the response.
-            var fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest).then(function(response) {
-                // Check if we received a valid response
-                if (
-                    !response ||
-                    response.status !== 200 ||
-                    response.type !== 'basic'
-                ) {
-                    return response;
-                }
-
-                // IMPORTANT: Clone the response. A response is a stream
-                // and because we want the browser to consume the response
-                // as well as the cache consuming the response, we need
-                // to clone it so we have two streams.
-                var responseToCache = response.clone();
-
-                caches.open(CACHE_NAME).then(function(cache) {
-                    cache.put(event.request, responseToCache);
-                });
-
-                return response;
-            });
         })
     );
 });
