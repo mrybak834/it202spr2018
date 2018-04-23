a5c04b1ba45b331a5f7ddf131ac164707bc1c5be
Marek Rybakiewicz <mrybak3@uic.edu>
2018-04-22 23:51:04 -0500

    final submission big project

--- projects/big/static/scripts/script.js
+++ projects/big/static/scripts/script.js
@@ -5862,49 +5862,6 @@ $(document).ready(function() {
         ]
     };
 
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/sw.js').then(
-                function(registration) {
-                    // Registration was successful
-                    console.log(
-                        'ServiceWorker registration successful with scope: ',
-                        registration.scope
-                    );
-                },
-                function(err) {
-                    // registration failed :(
-                    console.log('ServiceWorker registration failed: ', err);
-                }
-            );
-        });
-    }
-
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = ['./style.css', './script.css'];
-
-    self.addEventListener('install', function(event) {
-        // Perform install steps
-        event.waitUntil(
-            caches.open(CACHE_NAME).then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-        );
-    });
-
-    self.addEventListener('fetch', function(event) {
-        event.respondWith(
-            caches.match(event.request).then(function(response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-        );
-    });
-
     var latitude = 0;
     var longitude = 0;
     navigator.geolocation.getCurrentPosition(function(position) {
