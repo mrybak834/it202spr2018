746252a717addc2bf581f2e0fc8ca0aa25a06f48
Marek Rybakiewicz <mrybak3@uic.edu>
2018-04-22 23:40:30 -0500

    Third page

--- projects/big/static/scripts/script.js
+++ projects/big/static/scripts/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    /** MODAL */
+    // Get the modal
+    var modal = document.getElementById('resultsModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('view-results');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName('close')[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = 'none';
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    };
+    /** MODAL */
+
     console.log('ready!');
     var testMode = true;
     var userBlog = '';
@@ -5833,7 +5861,13 @@ $(document).ready(function() {
             }
         ]
     };
-    
+
+    var latitude = 0;
+    var longitude = 0;
+    navigator.geolocation.getCurrentPosition(function(position) {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+    });
 
     // Submit personal blog
     $('#user-submit').click(function() {
@@ -5857,6 +5891,16 @@ $(document).ready(function() {
 
         // Run all queries and analysis
         getResults();
+    });
+
+    // Submit comparison blog
+    $('#view-profile').click(function() {
+        // Hide comp and start page, show results page
+        $('#results').toggleClass('hidden');
+        $('#profile').toggleClass('hidden');
+
+        $('#locationDisplay').append('<p> Visiting from ' + longitude + ", " + latitude + '</p>');
+
     });
 
     function getResults() {
@@ -6119,6 +6163,7 @@ $(document).ready(function() {
     function sortProperties(obj) {
         // convert object into array
         var sortable = [];
+        sortable.push(['Type', 'Occurrence']);
         for (var key in obj)
             if (obj.hasOwnProperty(key)) sortable.push([key, obj[key]]); // each item is an array in format [key, value]
 
@@ -6140,6 +6185,46 @@ $(document).ready(function() {
         console.log('Comp Profile');
         console.log(compProfile);
 
-        $('#mdc-dialog-with-list').toggleClass('hidden');
+        /** Chart */
+        google.charts.load('current', { packages: ['corechart'] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        //create trigger to resizeEnd event
+        $(window).resize(function() {
+            if (this.resizeTO) clearTimeout(this.resizeTO);
+            this.resizeTO = setTimeout(function() {
+                $(this).trigger('resizeEnd');
+            }, 500);
+        });
+
+        //redraw graph when window resize is completed
+        $(window).on('resizeEnd', function() {
+            drawChart();
+        });
+
+        function drawChart() {
+            var userdata = google.visualization.arrayToDataTable(userProfile);
+            var compdata = google.visualization.arrayToDataTable(compProfile);
+            var options = {
+                title: 'User'
+            };
+
+            var chart = new google.visualization.PieChart(
+                document.getElementById('user-piechart')
+            );
+
+            chart.draw(userdata, options);
+
+            var options = {
+                title: 'Comparison'
+            };
+
+            var chart = new google.visualization.PieChart(
+                document.getElementById('comp-piechart')
+            );
+
+            chart.draw(compdata, options);
+        }
+        /** Chart */
     }
 });
